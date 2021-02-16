32465d63ca434a20ebcfbe16328ada5c1ef2de08
vovapipko <vpmail13@gmail.com>
2021-02-16 18:27:09 +0300

    Добавит скрипт генерации e-mail для интеграций

---
+++ Работа с формами/Обязательное поле E-mail для интеграций/script.js
@@ -0,0 +1,14 @@
+const getRandomEmail = max => (Math.random() * 10000).toFixed(4) + `@random-email.ru`;
+
+const setEmail = () => {
+    $(`form`).each(function (index, form) {
+        const emailValue = getRandomEmail();
+
+        $(form).find(`[data-type="email"]`).remove();
+        $(form).find(`.fields`).append(`<div class="field" data-type="email" style="display: none;"><div class="input"><input class="form-control text""></div></div>`);
+        $(form).find(`[data-type="email"] input`).val(emailValue);
+        console.log(emailValue, $(form).find(`[data-type="email"] input`));
+    });
+};
+
+document.addEventListener(`DOMContentLoaded`, setEmail);
